95a5d715aaf14e6ca7e1880141231e6f2eb91854
md_alif <md.alif10000@gmail.com>
2021-05-05 19:37:45 +0600

    ..

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,9 +1,9 @@
-export const api = "http://localhost:5000/";
-export const generatePublicUrl = (image) => {
-	return `${api}public/${image}`;
-};
-
-// export const api = "https://alif-blog-backend.herokuapp.com/";
+// export const api = "http://localhost:5000/";
 // export const generatePublicUrl = (image) => {
 // 	return `${api}public/${image}`;
 // };
+
+export const api = "https://alif-blog-backend.herokuapp.com/";
+export const generatePublicUrl = (image) => {
+	return `${api}public/${image}`;
+};
